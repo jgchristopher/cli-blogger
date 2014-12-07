c7c385115154c6b649a2c8a8ca6321de0a85338f
John Christopher <john.christopher@c2fo.com>
2014-12-06 20:41:42 -0600

    finalize move to canary fix model relationship to show author properly

--- app/models/post.js
+++ app/models/post.js
@@ -2,8 +2,8 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
-  author: DS.attr('string'),
   date: DS.attr('date'),
   excerpt: DS.attr('string'),
-  body: DS.attr('string')
+  body: DS.attr('string'),
+  author: DS.belongsTo('author')
 });
